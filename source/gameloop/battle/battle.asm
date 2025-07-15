1a4c2fc08d22bf151aad4176cf600b6c6257d972
sukus <52664542+sukus21@users.noreply.github.com>
2025-07-15 22:29:04 +0200

    make window segments controlable

--- source/gameloop/battle/battle.asm
+++ source/gameloop/battle/battle.asm
@@ -16,6 +16,8 @@ GameloopBattle::
     ld a, BATTLE_WINDOW_X_OPEN
     ld [wBattleDetailsX], a
     ld [wBattleMenuX], a
+    ld [wBattleDetailsXTarget], a
+    ld [wBattleMenuXTarget], a
 
     ; Transfer the required assets to VRAM
     vqueue_enqueue GameloopBattleInitTransfer
@@ -29,6 +31,62 @@ GameloopBattle::
     .loop
         ; Do things on the CPU
         call ReadInput
+        ld hl, wBattleMenuX
+
+        ; Read battle menu X -> D
+        ld a, [hl+]
+        ld d, a
+        ld a, BATTLE_WINDOW_X_OPEN
+        bit PADB_A, b
+        jr z, :+
+            ; have to add 10 or interpolation looks weird...
+            ; Clamp result after interpolation
+            ld a, BATTLE_WINDOW_X_CLOSED + 10
+        :
+        ld [hl-], a
+
+        ; Find difference between the two
+        sub a, d
+        jr z, :+
+            sra a
+            sra a
+            sra a
+        :
+        add a, d
+        cp a, BATTLE_WINDOW_X_CLOSED
+        jr c, :+
+            ld a, BATTLE_WINDOW_X_CLOSED
+        :
+        ld [hl+], a
+        inc hl
+
+        ; Read battle details X -> D
+        ld a, [hl+]
+        ld d, a
+        ld a, BATTLE_WINDOW_X_OPEN
+        ; ld b, 255
+        bit PADB_B, b
+        jr z, :+
+            ; have to add 10 or interpolation looks weird...
+            ; Clamp result after interpolation
+            ld a, BATTLE_WINDOW_X_CLOSED + 10
+        :
+        ld [hl-], a
+
+        ; Find difference between the two
+        sub a, d
+        jr z, :+
+            sra a
+            sra a
+            sra a
+        :
+        add a, d
+        cp a, BATTLE_WINDOW_X_CLOSED
+        jr c, :+
+            ld a, BATTLE_WINDOW_X_CLOSED
+        :
+        ld [hl+], a
+        inc hl
 
         ; Wait for Vblank
         .halting
@@ -56,9 +114,11 @@ SECTION "GAMELOOP BATTLE VARIABLES", WRAM0
 
 ; X-position of the top part of the menu
 wBattleMenuX:: ds 1
+wBattleMenuXTarget:: ds 1
 
 ; X-position 
 wBattleDetailsX:: ds 1
+wBattleDetailsXTarget:: ds 1
 
 ; Determines if battle menu is ready to be shown.
 wBattleMenuReady:: ds 1
