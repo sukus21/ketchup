fca56f0ccf5ca0da3609b3986342f317b4b838a8
sukus <52664542+sukus21@users.noreply.github.com>
2025-07-24 22:57:21 +0200

    function to test if player is allowed to go to tile

--- source/gameloop/battle/battle_functions.asm
+++ source/gameloop/battle/battle_functions.asm
@@ -1,3 +1,5 @@
+INCLUDE "struct/battle_stats.inc"
+INCLUDE "macro/relpointer.inc"
 
 
 SECTION "BATTLE HELPER FUNCTIONS", ROM0
@@ -46,6 +48,89 @@ SECTION "BATTLE HELPER FUNCTIONS", ROM0
         ld c, a
 
         ret
+    ;
+
+
+
+    ; Test if a player is allowed to move to the specified cell.
+    ;
+    ; Movement is allowed if:
+    ; * Cell is not occupied by another character
+    ; * Cell X < 3
+    ; * Cell X < 5 and >= 2
+    ;
+    ; Lives in ROM0.
+    ;
+    ; Input:
+    ; - `b`: Cell test X
+    ; - `c`: Cell test Y
+    ;
+    ; Returns:
+    ; - `a`: 0 if allowed
+    ; - `fZ`: Set if allowed (z = allowed)
+    ;
+    ; Destroys: `af`
+    BattleCanPlayerMoveTo::
+
+        ; X must be below 3
+        ld a, b
+        cp a, 3
+        jr c, :+
+            or a, $FF ; reset Z flag
+            ret
+        :
+        
+        ; Y must be below 5
+        ld a, c
+        cp a, 5
+        jr c, :+
+            or a, $FF ; reset Z flag
+            ret
+        :
+
+        ; Y must also be above 1
+        cp a, 2
+        jr nc, :+
+            or a, $FF ; reset Z flag
+            ret
+        :
+
+        ; Save these for later
+        push hl
+
+        ; Check overlap
+        ld hl, wBattleStatsDuffin
+        call .helper
+        ld hl, wBattleStatsMenja
+        call nz, .helper
+        ld hl, wBattleStatsHerbert
+        call nz, .helper
+
+        ; Return
+        xor a, $FF ; invert Z flag
+        pop hl
+        ret
+
+        ; Input:
+        ; - `bc`: Test coordinates
+        ; - `hl`: `BATTLE_STATS_T` pointer
+        ;
+        ; Returns:
+        ; - `a`: 0 if occupied
+        ;
+        ; Destroys: `hl`, `af`
+        .helper
+            relpointer_init l
+            relpointer_move BATTLE_STATS_X
+            ld a, b
+            sub a, [hl]
+            ret nz
+            relpointer_move BATTLE_STATS_Y
+            ld a, c
+            sub a, [hl]
+            relpointer_destroy
+            ret
+        ;
     ;
 
 ENDSECTION
